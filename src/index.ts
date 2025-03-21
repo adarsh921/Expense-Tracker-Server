1076f4f88ec1282cfb89c16b72ac3c89cf8adcf5
Kumar Adarsh <kradarsh24jan@gmail.com>
2025-03-21 11:52:02 +0530

    changes pushed

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 if (!process.env.MONGO_URI) {
   throw new Error("mongoURL is not defined in enviorment variables");
 }
-const MONGO_URI: string = process.env.MONGO_URI;
+const mongoURL: string = process.env.MONGO_URI;
 
 const port = process.env.PORT;
 
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 });
 
 mongoose
-  .connect(MONGO_URI)
+  .connect(mongoURL)
   .then(() => {
     console.log("Connected to MONGO!");
   })
